b4935a36d2af97269b091064830ac01ca49f7ab2
Raktim Kalita <155469713+Rktim@users.noreply.github.com>
2025-07-18 12:36:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="balens",
-    version="0.0.1",
+    version="0.0.2",
     description="A universal Python toolkit for detecting and fixing data imbalance.",
     author="Raktim Kalita",
     author_email="raktimkalita.ai@gmail.com",
